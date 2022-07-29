5878ad22e9ca3f59026c1a8893a93bcb468b14fa
SHARBEL KALLOUMAH <sharbel1999x@gmail.com>
2022-07-29 20:12:00 +0300

    face tracking last edits +30fps

--- face2.py
+++ face2.py
@@ -15,8 +15,11 @@ from keras_vggface.utils import preprocess_input
 
 from face_detector import Detector
 
+import numpy as np
 import time
-
+import mediapipe as mp
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
 
 detector = Detector()
 # model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
@@ -141,3 +144,160 @@ def detect_faces(img, match):
             cv2.putText(img, 'Stranger', p1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     return img, len(res)
+
+class Tracker(object):
+    def __init__(self, dist_thresh=50, max_humans=6):
+        self._dist_thresh = dist_thresh
+        self._max_humans = max_humans
+
+        self._dict_id2skeleton = {}
+        self._cnt_humans = 0
+
+    def track(self, curr_skels):
+        #         curr_skels = self._sort_skeletons_by_dist_to_center(curr_skels) #########
+        N = len(curr_skels)
+        # Match skeletons between curr and prev
+        if len(self._dict_id2skeleton) > 0:
+            ids, prev_skels = map(list, zip(*self._dict_id2skeleton.items()))
+            good_matches = self._match_features(prev_skels, curr_skels)
+
+            self._dict_id2skeleton = {}
+            is_matched = [False] * N
+            for i2, i1 in good_matches.items():
+                human_id = ids[i1]
+                self._dict_id2skeleton[human_id] = np.array(curr_skels[i2])
+                is_matched[i2] = True
+            unmatched_idx = [i for i, matched in enumerate(
+                is_matched) if not matched]
+        else:
+            good_matches = []
+            unmatched_idx = range(N)
+
+        # Add unmatched skeletons (which are new skeletons) to the list
+        num_humans_to_add = min(len(unmatched_idx),
+                                self._max_humans - len(good_matches))
+        #         print("num:",num_humans_to_add)
+        for i in range(num_humans_to_add):
+            self._cnt_humans += 1
+            self._dict_id2skeleton[self._cnt_humans] = np.array(
+                curr_skels[unmatched_idx[i]])
+        #         print(self._dict_id2skeleton)
+        return self._dict_id2skeleton
+
+    def _get_neck(self, skeleton):
+        x, y = skeleton[2], skeleton[3]
+        return x, y
+
+    def _match_features(self, features1, features2):
+        features1, features2 = np.array(features1), np.array(features2)
+
+        cost = lambda x1, x2: np.linalg.norm(x1 - x2)
+
+        def calc_dist(p1, p2):
+            return (
+                           (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
+
+        def cost(sk1, sk2):
+
+            # neck, shoulder, elbow, hip, knee
+            #             joints = np.array([2, 3, 4, 5, 6, 7, 10, 11, 12,  ################################
+            #                                13, 16, 14, 14, 14, 14, 14, 14, 14])
+
+            #             joints = np.array([2, 3, 4, 5, 6, 7,14])
+            joints = np.array([0, 1, 2])
+
+            #             print("joints:",sk1[joints])
+            sk1, sk2 = sk1[joints], sk2[joints]
+            valid_idx = np.logical_and(sk1 != 0, sk2 != 0)
+            sk1, sk2 = sk1[valid_idx], sk2[valid_idx]
+            sum_dist, num_points = 0, int(len(sk1) / 2)
+            if num_points == 0:
+                return 99999
+            else:
+                for i in range(num_points):  # compute distance between each pair of joint
+                    idx = i * 2
+                    sum_dist += calc_dist(sk1[idx:idx + 2], sk2[idx:idx + 2])
+                mean_dist = sum_dist / num_points
+                mean_dist /= (1.0 + 0.05 * num_points)  # more points, the better
+                return mean_dist
+
+        # If f1i is matched to f2j and vice versa, the match is good.
+        good_matches = {}
+        n1, n2 = len(features1), len(features2)
+        if n1 and n2:
+
+            # dist_matrix[i][j] is the distance between features[i] and features[j]
+            dist_matrix = [[cost(f1, f2) for f2 in features2]
+                           for f1 in features1]
+            dist_matrix = np.array(dist_matrix)
+
+            # Find the match of features1[i]
+            matches_f1_to_f2 = [dist_matrix[row, :].argmin()
+                                for row in range(n1)]
+
+            # Find the match of features2[i]
+            matches_f2_to_f1 = [dist_matrix[:, col].argmin()
+                                for col in range(n2)]
+
+            for i1, i2 in enumerate(matches_f1_to_f2):
+                if matches_f2_to_f1[i2] == i1 and dist_matrix[i1, i2] < self._dist_thresh:
+                    good_matches[i2] = i1
+            #                     print("goooooog")
+
+            if 0:
+                print("distance matrix:", dist_matrix)
+                print("matches_f1_to_f2:", matches_f1_to_f2)
+                print("matches_f1_to_f2:", matches_f2_to_f1)
+                print("good_matches:", good_matches)
+
+        return good_matches
+
+
+face_detection = mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5)
+tracker = Tracker()
+person_ids = []
+def detect_faces_media(image, match):
+    check_face = False
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = face_detection.process(image)
+
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    if results.detections:
+        for detection in results.detections:
+            x1 = int(detection.location_data.relative_bounding_box.xmin*480)
+            x2 = int(x1 + detection.location_data.relative_bounding_box.width*480)
+            y1 = int(detection.location_data.relative_bounding_box.ymin*360)
+            y2 = int(y1 + detection.location_data.relative_bounding_box.height*360)
+            p1 = (x1, y1)
+            p2 = (x2, y2)
+            image = cv2.rectangle(image, p1, p2, (255, 10, 10))
+            face = np.array([[[x1, y1], [x2, y2], [(x1 + x2) / 2, (y1 + y2) / 2]]])
+            fa = tracker.track(face)
+            new_ids = list(fa.keys())
+
+            for id in new_ids:
+                if id in person_ids:
+                    break
+                else:
+                    person_ids.append(id)
+                    check_face = True
+
+
+
+        if match is not None:
+            cv2.putText(image, match, p1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        else:
+            cv2.putText(image, 'Stranger :  '+str(list(fa.keys())[0]), p1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    if results.detections is None:
+        length = 0
+    else:
+        length = len(results.detections)
+
+
+        # if results.detections:
+        #     im = image[y1:y2, x1:x2]
+
+    return image,check_face,length
--- test.py
+++ test.py
@@ -2,33 +2,41 @@ import cv2
 # from fcm import send_notification
 # from fight_fall import fall_fight_model
 # from fire import is_fire
-from face2 import get_embedding, match, detect_faces
+from face2 import get_embedding, match, detect_faces,detect_faces_media
 import urllib.request
 import numpy as np
 import time
 
 URL = "http://192.168.1.109:8080/shot.jpg"
 fps_time = 0
+cap = cv2.VideoCapture(0)
+
+_match = None
+res = {
+    "face": None,
+}
 
 while True:
-    img_arr = np.array(bytearray(urllib.request.urlopen(URL).read()), dtype=np.uint8)
-    img = cv2.imdecode(img_arr, -1)
+
+    ret, img = cap.read()
+    #img_arr = np.array(bytearray(urllib.request.urlopen(URL).read()), dtype=np.uint8)
+    #img = cv2.imdecode(img_arr, -1)
     img = cv2.resize(img, (480, 360))
 
 
-    _match = match(img)
+
     # _is_fire = is_fire(img)
     # _fall_fight = fall_fight_model().detect(img)
+    img, check_face, faces_count = detect_faces_media(img, res['face'])
+    print(check_face)
+    if check_face:
+        _match = match(img)
 
     res = {
         "face": _match,
         # "fire": _is_fire,
         # "fall": _fall_fight,
     }
-
-    print(res)
-
-    img, faces_count = detect_faces(img, res['face'])
 
     # Notifications
     # if _match is not None:
@@ -46,7 +54,9 @@ while True:
 
     cv2.putText(img, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     cv2.imshow('IPWebcam', img)
+    print((1.0 / (time.time() - fps_time)))
     fps_time = time.time()
+
 
 
     q = cv2.waitKey(1)
