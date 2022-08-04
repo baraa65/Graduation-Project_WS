95b38b2ea1ed1ad023265e579a8c89c98d0940c6
baraa65 <baraash65@hotmail.com>
2022-08-04 23:18:45 +0300

    fix media urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+
 # urls
+from mysite import settings
+
 urlpatterns = [
     path('api/v1/images/', include('images.urls')),
     path('api/v1/schedules/', include('schedules.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
